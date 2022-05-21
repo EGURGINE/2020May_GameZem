0f65ff0702fe96d6a9435fc692fc838ede6a730c
EGURGINE <93648712+EGURGINE@users.noreply.github.com>
2022-05-21 18:08:47 +0900

    슬라임 리소스 적용

--- Assets/Junho/Scripts/Enemy.cs
+++ Assets/Junho/Scripts/Enemy.cs
@@ -16,6 +16,11 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Damage(1);
+        }
+        transform.LookAt(new Vector3(target.transform.position.x,0,target.transform.position.z));
         transform.position = Vector3.MoveTowards(transform.position, target.transform.position, spd * Time.deltaTime);
     }
     public void Damage(float dmg)
@@ -23,12 +28,8 @@ public class Enemy : MonoBehaviour
         hp -= dmg;
         if (hp <= 0)
         {
+            Instantiate(deadPcy).transform.position = transform.position;
             Destroy(gameObject);
         }
     }
-    private void OnDestroy()
-    {
-        Instantiate(deadPcy).transform.position = transform.position;
-    }
-   
 }
