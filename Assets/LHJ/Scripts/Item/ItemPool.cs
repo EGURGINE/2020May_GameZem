a908900962fef26ee0ac129570d0cf4033519ad7
imhyunjun <limhyunjun04@naver.com>
2022-05-22 09:50:09 +0900

    빌드 셋팅

--- Assets/LHJ/Scripts/Item/ItemPool.cs
+++ Assets/LHJ/Scripts/Item/ItemPool.cs
@@ -15,13 +15,14 @@ public class ItemPool : ObjectPool<Item>
         itemList.Add(new Tuple<ItemType, float>(ItemType.Bullet, 3));   //딸기
         itemList.Add(new Tuple<ItemType, float>(ItemType.Bullet, 5));   //참외
         itemList.Add(new Tuple<ItemType, float>(ItemType.Score, 300));   //토마토
-        itemList.Add(new Tuple<ItemType, float>(ItemType.HP, 10));   //바나나
-        itemList.Add(new Tuple<ItemType, float>(ItemType.HP, 5));   //수박
+        itemList.Add(new Tuple<ItemType, float>(ItemType.HP, -10));   //바나나
+        itemList.Add(new Tuple<ItemType, float>(ItemType.HP, -5));   //수박
+        itemList.Add(new Tuple<ItemType, float>(ItemType.HP, 5));   //덜 익은 딸기
     }
 
     public void SetItemRandom(Vector3 _pos)
     {
-        int a = UnityEngine.Random.Range(0, 4);
+        int a = UnityEngine.Random.Range(0, 6);
         Item item = GetObj(_pos);
         item.SetItem(itemList[a].Item1, itemList[a].Item2);
         item.GetComponent<MeshFilter>().mesh = meshes[a];
